2f69f72f9ac0bf94ceb38ea068af22b0ee21e944
Artem Krasov <artkrasov@gmail.com>
2021-10-21 17:41:05 +0300

    Added navigation on page

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,16 @@ window.onload = function() {
         }
     }
 };
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
